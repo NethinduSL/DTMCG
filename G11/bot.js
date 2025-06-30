b00d7542f421d0dd1d4e0a3c7e11cb8842d1d3f6
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2025-07-01 02:26:24 +0530

    Update bot.js

--- G11/bot.js
+++ G11/bot.js
@@ -6,8 +6,13 @@
     { start: "8:31", end: "9:10", id: "period2" },
     { start: "9:11", end: "9:50", id: "period3" },
     { start: "9:51", end: "9:59", id: "period4" },
-            { start: "10:00", end: "10:30", id: "period4" },
-    { start: "10:31", end: "10:50", id: "Interval" }, // Interval
+    { start: "10:00", end: "10:30", id: "period4" },
+
+
+    { start: "10:31", end: "10:50", id: "period5" }, // Interval
+
+
+    
     { start: "10:51", end: "11:30", id: "period6" },
     { start: "11:31", end: "12:10", id: "period7" },
     { start: "12:11", end: "12:50", id: "period8" },
